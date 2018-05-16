3a88080abdbcdfcd954d2d384f01bfb4b3e62a27
keotsei <keo34@cornell.edu>
2018-05-16 12:45:54 -0500

    free Agents Button Change
    Added javascript for button change with the create new group option

--- createGroup/freeAgents.js
+++ createGroup/freeAgents.js
@@ -2,15 +2,30 @@
 var tracklist = ["Let It Happen", "Nangs", "The Moment", "Yes I'm Changing", "Eventually", "Gossip", "The Less I Know The Better", "Past Life", "Disciples", "'Cause I'm A Man"];
 var grouplist="Alex Anders"
 
+var checkboxChecker = setInterval(checkUpdate,100);
+
 
 function init() {
 	// Your code goes here
-
 };
 
-function volUp() {
-	// Your code goes here
-
+function checkUpdate() {
+    var ids = document.getElementsByClassName("form-check-input")
+    var count = 0;
+    for (i = 0; i < ids.length; i++) {
+        var x = ids[i].checked;
+        if (x == true) {
+            count = count + 1;
+        } 
+    }
+    
+    if (count > 0) {
+        document.getElementById("createNewGroup").innerHTML = "Form New Group"
+        document.getElementById("createNewGroup").setAttribute('href',"groupConfirmed.html")
+    } else {
+        document.getElementById("createNewGroup").innerHTML = "Create New Group"
+        document.getElementById("createNewGroup").setAttribute('href',"createGroup.html")
+    }
 }
 
 init();
