e6ad55aa5a5f21bb5088134a55cbe8cdf33dfa9f
keotsei <keo34@cornell.edu>
2018-05-23 12:21:13 -0500

    Change displayed books
    js to change the books that are displayed

--- findTextbook/findTextbook.js
+++ findTextbook/findTextbook.js
@@ -2,6 +2,23 @@
 var dayList = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 var grouplist="Alex Anders"
 
+var monList = [["History 255","History 255","History 301"],
+               ["Something about textbook","Something about textbook","Something about textbook"]];
+var tuesList = [["History 250","History 260","History 301"],
+               ["Something about textbook","Something about textbook","Something about textbook"]];
+var wedList = [["History 260","History 260"],
+               ["Something about textbook","Something about textbook"]];
+var thursList = [[],
+               []];
+var friList = [["History 250","History 250","History 250","History 260","History 260","History 301"],
+               ["Something about textbook","Something about textbook","Something about textbook","Something about textbook","Something about textbook","Something about textbook"]];
+var satList = [["History 260","History 260","History 301","History 301","History 301"],
+               ["Something about textbook","Something about textbook","Something about textbook","Something about textbook","Something about textbook"]];
+var sunList = [["History 250","History 250","History 250","History 250","History 250","History 250"],
+               ["Something about textbook","Something about textbook","Something about textbook","Something about textbook","Something about textbook","Something about textbook"]];
+
+
+
 var day_num;
 
 
@@ -18,6 +35,8 @@ function rightDay() {
         day_num = day_num + 1;
     }
     dayOfWeek.innerHTML = dayList[day_num];
+
+    refreshDay()
 }
 
 function leftDay() {
@@ -29,22 +48,242 @@ function leftDay() {
     }
     dayOfWeek.innerHTML = dayList[day_num];
     
-    //refreshDay()
+    refreshDay()
 }
 
 function refreshDay() {
     var dayOfWeek = document.getElementById("dayOfWeek");
-    var book_entries = document.getElementsByClassName("row findGroupEntry")
+    var book_entries = document.getElementsByClassName("row findGroupEntry");
     var length_max = book_entries.length;
     for (i = length_max-1; i >= 0; i--) {
         book_entries[i].remove();
     }
 
-    var course_entries = document.getElementsByClassName("findCourseEntries")[0]
 
-    //For now just add as many entries as the day
-    for (i = 0; i < day_num + 2; i++){
-        course_entries.createElement("div")
+    if (day_num == 0) {
+        addMonday()
+    } else if (day_num == 1) {
+        addTuesday()
+    } else if (day_num == 2) {
+        addWednesday()
+    } else if (day_num == 3) {
+        addThursday()
+    } else if (day_num == 4) {
+        addFriday()
+    } else if (day_num == 5) {
+        addSaturday()
+    } else if (day_num == 6) {
+        addSunday()
+    }
+}
+
+function addMonday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < monList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = monList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = monList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addTuesday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < tuesList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = tuesList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = tuesList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addWednesday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < wedList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = wedList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = wedList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addThursday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < thursList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = thursList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = thursList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addFriday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < friList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = friList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = friList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addSaturday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < satList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = satList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = satList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
+    }
+}
+
+function addSunday() {
+    var courseEntries = document.getElementsByClassName("findCourseEntries")[0];
+
+    for (i = 0; i < sunList[0].length; i++){
+        var top = document.createElement("div")
+        top.className = "row findGroupEntry"
+
+        var title = document.createElement("p");
+        title.innerHTML = sunList[0][i];
+        var a = document.createElement("a");
+        a.setAttribute('href',"selectBook.html");
+        a.appendChild(title);
+
+        var name = document.createElement("div")
+        name.className = "col-4 text-center"
+        name.appendChild(a)
+        top.appendChild(name) 
+
+        var info = document.createElement("p");
+        info.innerHTML = sunList[1][i];
+
+        var info_top = document.createElement("div")
+        info_top.className = "col-8"
+        info_top.appendChild(info)
+        top.appendChild(info_top) 
+
+        courseEntries.appendChild(top)
     }
 }
 
