bcb01165ed792b6a597be5c80ac78b188fd37e64
keotsei <keo34@cornell.edu>
2018-05-25 12:00:52 -0500

    Added pages for lend and borrow schedules
    Added pages for lend and borrow schedules

--- profile/editProfile.js
+++ profile/editProfile.js
@@ -2,9 +2,10 @@
 function storeLink(finish_link) {
 
     if (finish_link == 0) {
-        localStorage.setItem("changeScheduleLink",finish_link)
+        localStorage.setItem("changeScheduleLink","../profile/lendScheduleUpdate.html")
 
-
+        //TODO: match variable names profileShare_ with internal profile data
+        //TODO: javaScript for updating shared profile variables after schedule
         var mon = checkNull(localStorage.getItem("profileShareMon"));
         var tues = checkNull(localStorage.getItem("profileShareTues"));
         var wed = checkNull(localStorage.getItem("profileShareWed"));
@@ -20,6 +21,24 @@ function storeLink(finish_link) {
         localStorage.setItem("satAvail",sat)
         localStorage.setItem("sunAvail",sun)
         localStorage.setItem("curDay",0)
+    } else {
+        localStorage.setItem("changeScheduleLink","../profile/borrowScheduleUpdate.html")
+
+        var mon = checkNull(localStorage.getItem("profileShareMon"));
+        var tues = checkNull(localStorage.getItem("profileShareTues"));
+        var wed = checkNull(localStorage.getItem("profileShareWed"));
+        var thurs = checkNull(localStorage.getItem("profileShareThurs"));
+        var fri = checkNull(localStorage.getItem("profileShareFri"));
+        var sat = checkNull(localStorage.getItem("profileShareSat"));
+        var sun = checkNull(localStorage.getItem("profileShareSun"));    
+        localStorage.setItem("monAvail",mon)
+        localStorage.setItem("tuesAvail",tues)
+        localStorage.setItem("wedAvail",wed)
+        localStorage.setItem("thursAvail",thurs)
+        localStorage.setItem("friAvail",fri)
+        localStorage.setItem("satAvail",sat)
+        localStorage.setItem("sunAvail",sun)
+        localStorage.setItem("curDay",0)   
     }
 }
 
