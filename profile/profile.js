752af248b9a275a386cf3c3b108d9487ec4529ec
leeks <lilim2019@u.northwestern.edu>
2018-05-29 17:51:44 -0500

    updates

--- profile/profile.js
+++ profile/profile.js
@@ -2,3 +2,11 @@
 function sendBorrow(borrow) {
     localStorage.setItem("borrow",borrow)
 }
+
+function highlightShelf(shelf) {
+	shelf.style.opacity = 0.5;
+}
+
+function unhighlightShelf(shelf) {
+	shelf.style.opacity = 1.0;
+}
